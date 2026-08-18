56e34225879210de5d0be2b2a1bfc16c19c6bd8e
manuelixvivi <manuelrg0906.putra@gmail.com>
2026-08-18 08:11:04 +0700

    Remove mangum dependency from api/index.py and clean vercel.json

--- api/index.py
+++ api/index.py
@@ -6,10 +6,7 @@ root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
-from mangum import Mangum
 from main import app
 
 # Top-level ASGI handler for Vercel Serverless Functions
 app = app
-handler = Mangum(app)
-application = app
